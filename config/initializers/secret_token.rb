765e2e3b19398f2f1d2daf44553e832b29ae7ec5
laudbrian <laudbrian@gmail.com>
2014-06-24 13:50:12 -0700

    Created Rails application with status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Briansbook::Application.config.secret_key_base = '9f6f91cfe59b1cfc4a4fc5f5b7f3fb6033c8332836e3a568acd555cf5ebc744014256c1fbad229273fa076e1f9027274c3f4d189f63f2241127488044aaff845'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Briansbook::Application.config.session_store :cookie_store, key: '_briansbook_session'
